59dbf6e0415f2f3b51cb77593622e79074ae8f1d
Maja Frankiewicz <majafrank@live.se>
2021-10-02 16:03:29 +0200

    Added query for creating props as nodes

--- parse_data.py
+++ parse_data.py
@@ -22,7 +22,7 @@ import pickle
 m = io.open('./datasets.json', 'r', encoding='utf-8').read()
 # Dump all data from BFS file as string, then loads as json object
 n = json.loads(m)
-print(n)
+# print(n)
 for dataset in n:
     # print(type(dataset))
 
@@ -31,11 +31,27 @@ for dataset in n:
         if attr[0] == '@':
             # print(attr)
             attr = 'at_'+attr[1:]
-            # attr.replace('@', 'at_')
         if type(value) == str:
             dataset_node += attr+":'"+value+"',"
+        if type(value) == list:
+            # print(value)
+            attr_label = attr[0].upper()+attr[1:]
+            if type(value[0]) == str:
+                # print(value)
+                new_node = ""
+                for str_element in value:
+                    # print(str_element)
+                    new_node += " MERGE (n:" + attr_label + \
+                        " {title:'"+str_element+"'})"
+                print(new_node)
+            # for element in value:
+                # if type(element) == string:
+
+                # new_node += ""
+
     dataset_node = dataset_node[:-1]+"})"
+
     # print(attr, value)
     # dataset_node += "})"
 
-    print(dataset_node)
+    # print(dataset_node)
