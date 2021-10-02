cea76f6aafaf46aec3e69e8bb3dde36014d9e6df
Maja Frankiewicz <majafrank@live.se>
2021-10-02 17:47:39 +0200

    started w rel queries

--- parse_data.py
+++ parse_data.py
@@ -39,8 +39,11 @@ for dataset in n:
     dataset_node = "MERGE (a:Dataset {"
 
     new_node_list_str = ""
+    count_l_str = "0"
     new_node_list_obj = ""
-    new_node_obj = ""
+    count_l_obj = "0"
+    new_node_object = ""
+    count_obj = "0"
 
     # iterating key-value pairs
     for attr, value in dataset.items():
@@ -55,39 +58,48 @@ for dataset in n:
         # each of the elements with the property name as label
         if type(value) == list:
             if type(value[0]) == str:
-                new_node_list_str = ""
+                # new_node_list_str = ""
                 for str_element in value:
                     # print(str_element)
-                    new_node_list_str += " MERGE (n:" + attr_label + \
+                    new_node_list_str += " MERGE (n"+count_l_str+":" + attr_label + \
                         " {title:'"+str_element+"'})"
+                    count_l_str = str(int(count_l_str) + 1)
                 # print(new_node)
 
             # if the elements in list are objects, then create new nodes for
             # each of the elements with the key-value pairs as properties and the property name as Label
             if type(value[0]) == dict:
-                new_node_list_obj = ""
+                # new_node_list_obj = ""
                 for dict_element in value:
-                    new_node_list_obj += " MERGE (n:" + attr_label + " {"
+                    new_node_list_obj += " MERGE (n" + \
+                        count_l_obj+":" + attr_label + " {"
                     for dict_attr, dict_value in dict_element.items():
                         new_node_list_obj += dict_attr+":'"+dict_value+"', "
                     new_node_list_obj = new_node_list_obj[:-2]+"})"
+                    count_l_obj = str(int(count_l_obj) + 1)
+
                 # print(new_node)
 
         if type(value) == dict:
-            new_node_object = ""
+            # new_node_object = ""
 
-            new_node_object += " MERGE (n:" + attr_label + " {"
+            new_node_object += " MERGE (n"+count_obj+":" + attr_label + " {"
             for dict_attr, dict_value in value.items():
                 dict_attr = check_if_at(dict_attr)
                 new_node_object += dict_attr+":'"+dict_value+"', "
-            new_node_object = new_node_object[:-2]+"})"
+            new_node_object = new_node_object[:-2] + \
+                "}) MERGE (n"+count_obj + \
+                ")<-[:HAS_"+attr_label.upper() + "]-(a)"
+            # print(new_node_object)
+            count_obj = str(int(count_obj) + 1)
 
     # print(dataset_node)
+    # print(new_node_object)
     print(new_node_object)
 
     print()
 
-    dataset_node = dataset_node[:-1]+"})"
+    dataset_node = dataset_node[: -1]+"})"
 
     # print(attr, value)
     # dataset_node += "})"
