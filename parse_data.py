0afebfdc562959a0c71e9369a7a443e2a5989325
Maja Frankiewicz <majafrank@live.se>
2021-10-02 16:54:25 +0200

    Added query for property with object

--- parse_data.py
+++ parse_data.py
@@ -24,30 +24,62 @@ m = io.open('./datasets.json', 'r', encoding='utf-8').read()
 n = json.loads(m)
 # print(n)
 for dataset in n:
-    # print(type(dataset))
-
     dataset_node = "MERGE (:Dataset {"
+
+    # iterating key-value pairs
     for attr, value in dataset.items():
         if attr[0] == '@':
-            # print(attr)
+
+            # replacing symbol since neo4j doesnt accept @-symbools as property name
             attr = 'at_'+attr[1:]
+
+        # if property value is a string, add as property in Dataset node
         if type(value) == str:
             dataset_node += attr+":'"+value+"',"
+
+        # if property value is a list, and the elements are a string, then create new nodes for
+        # each of the elements with the property name as label
+        # if the lements are an object, then create new nodes for
+        # each of the elements with the key-value pairs as properties an the property name as Label
         if type(value) == list:
-            # print(value)
             attr_label = attr[0].upper()+attr[1:]
             if type(value[0]) == str:
-                # print(value)
                 new_node = ""
                 for str_element in value:
                     # print(str_element)
                     new_node += " MERGE (n:" + attr_label + \
                         " {title:'"+str_element+"'})"
-                print(new_node)
-            # for element in value:
-                # if type(element) == string:
+                # print(new_node)
 
-                # new_node += ""
+            if type(value[0]) == dict:
+                new_node = ""
+                attr_label = attr[0].upper()+attr[1:]
+
+                for dict_element in value:
+                    new_node += " MERGE (n:" + attr_label + " {"
+                    for dict_attr, dict_value in dict_element.items():
+                        if dict_attr[0] == '@':
+                            dict_attr = 'at_'+dict_attr[1:]
+
+                        new_node += dict_attr+":'"+dict_value+"', "
+                    new_node = new_node[:-2]+"})"
+                # print(new_node)
+
+        if type(value) == dict:
+            attr_label = attr[0].upper()+attr[1:]
+            new_node_object = ""
+            new_node_object += " MERGE (n:" + attr_label + " {"
+            for dict_attr, dict_value in value.items():
+                dict_attr = 'at_'+dict_attr[1:]
+
+                new_node_object += dict_attr+":'"+dict_value+"', "
+            new_node_object = new_node_object[:-2]+"})"
+
+            print(new_node_object)
+            # for element in value:
+            # if type(element) == string:
+
+            # new_node += ""
 
     dataset_node = dataset_node[:-1]+"})"
 
