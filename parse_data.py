0486a7fb8d79f20e69f3a9498f1bdf1cc39167dc
Maja Frankiewicz <majafrank@live.se>
2021-10-02 15:26:38 +0200

    Added parsing code

--- parse_data.py
+++ parse_data.py
@@ -8,34 +8,28 @@ import pickle
 
 # p_file = 'words_pickle.p'
 
-r = requests.get('https://data.nasa.gov/data.json')
-
-r_datasets = r.json()['dataset']
-
-o = io.open('./datasets.json', 'a', encoding='utf-8')
+# r = requests.get('https://data.nasa.gov/data.json')
+# r_datasets = r.json()['dataset'][:100]
+# o = io.open('./datasets.json', 'a', encoding='utf-8')
 
 
 # for dataset in r_datasets:
 
-#     # Open file with BFS (that is not modified with guids)
-# m = io.open('./bfs/'+bfs['id']+'.json', 'r', encoding='utf-8').read()
-# # Dump all data from BFS file as string, then loads as json object
-# n = json.loads(json.dumps(m))
-# # Delete existing modified BFS file to create a new one
-# os.remove('./bfs_br.ai/'+bfs['id']+'.json')
-# # Open new file with "append"
-# # Convert json objects to python dictionary
-# outfile = eval(n)
 
-# First parameters in first object before loop
-# print(r_datasets)
-json.dump(r_datasets, o)
+# json.dump(r_datasets, o)
 
+# Open file with BFS (that is not modified with guids)
+m = io.open('./datasets.json', 'r', encoding='utf-8').read()
+# Dump all data from BFS file as string, then loads as json object
+n = json.loads(json.loads(json.dumps(m)))
+for dataset in n:
+    # print(type(dataset))
 
-# print(r.json()['userId'])
-# print(r.json()['conformsTo'])
-# r.status_code
-# r.headers['content-type']
-# r.encoding
-# r.text
-# r.json()
+    dataset_node = "MEGRE (:Dataset {"
+    for attr, value in dataset.items():
+        if type(value) == String:
+            dataset_node += attr+":"+value+","
+        # print(attr, value)
+    dataset_node += "})"
+
+    print(dataset_node)
