f8668ce57feee910156a792a6f8fd6c3160ffa3b
Maja Frankiewicz <majafrank@live.se>
2021-10-02 17:23:39 +0200

    cleaned code

--- parse_data.py
+++ parse_data.py
@@ -6,6 +6,18 @@ import json
 import uuid
 import pickle
 
+
+def check_if_at(attr):
+    '''replacing symbol since neo4j doesnt accept @-symbools as property name'''
+
+    if attr[0] == '@':
+        attr = 'at_'+attr[1:]
+    return attr
+
+
+def get_relationship(name):
+    return name
+
 # p_file = 'words_pickle.p'
 
 # r = requests.get('https://data.nasa.gov/data.json')
@@ -24,62 +36,56 @@ m = io.open('./datasets.json', 'r', encoding='utf-8').read()
 n = json.loads(m)
 # print(n)
 for dataset in n:
-    dataset_node = "MERGE (:Dataset {"
+    dataset_node = "MERGE (a:Dataset {"
+
+    new_node_list_str = ""
+    new_node_list_obj = ""
+    new_node_obj = ""
 
     # iterating key-value pairs
     for attr, value in dataset.items():
-        if attr[0] == '@':
-
-            # replacing symbol since neo4j doesnt accept @-symbools as property name
-            attr = 'at_'+attr[1:]
+        attr = check_if_at(attr)
 
         # if property value is a string, add as property in Dataset node
         if type(value) == str:
             dataset_node += attr+":'"+value+"',"
+        attr_label = attr[0].upper()+attr[1:]
 
         # if property value is a list, and the elements are a string, then create new nodes for
         # each of the elements with the property name as label
-        # if the lements are an object, then create new nodes for
-        # each of the elements with the key-value pairs as properties an the property name as Label
         if type(value) == list:
-            attr_label = attr[0].upper()+attr[1:]
             if type(value[0]) == str:
-                new_node = ""
+                new_node_list_str = ""
                 for str_element in value:
                     # print(str_element)
-                    new_node += " MERGE (n:" + attr_label + \
+                    new_node_list_str += " MERGE (n:" + attr_label + \
                         " {title:'"+str_element+"'})"
                 # print(new_node)
 
+            # if the elements in list are objects, then create new nodes for
+            # each of the elements with the key-value pairs as properties and the property name as Label
             if type(value[0]) == dict:
-                new_node = ""
-                attr_label = attr[0].upper()+attr[1:]
-
+                new_node_list_obj = ""
                 for dict_element in value:
-                    new_node += " MERGE (n:" + attr_label + " {"
+                    new_node_list_obj += " MERGE (n:" + attr_label + " {"
                     for dict_attr, dict_value in dict_element.items():
-                        if dict_attr[0] == '@':
-                            dict_attr = 'at_'+dict_attr[1:]
-
-                        new_node += dict_attr+":'"+dict_value+"', "
-                    new_node = new_node[:-2]+"})"
+                        new_node_list_obj += dict_attr+":'"+dict_value+"', "
+                    new_node_list_obj = new_node_list_obj[:-2]+"})"
                 # print(new_node)
 
         if type(value) == dict:
-            attr_label = attr[0].upper()+attr[1:]
             new_node_object = ""
+
             new_node_object += " MERGE (n:" + attr_label + " {"
             for dict_attr, dict_value in value.items():
-                dict_attr = 'at_'+dict_attr[1:]
-
+                dict_attr = check_if_at(dict_attr)
                 new_node_object += dict_attr+":'"+dict_value+"', "
             new_node_object = new_node_object[:-2]+"})"
 
-            print(new_node_object)
-            # for element in value:
-            # if type(element) == string:
+    # print(dataset_node)
+    print(new_node_object)
 
-            # new_node += ""
+    print()
 
     dataset_node = dataset_node[:-1]+"})"
 
