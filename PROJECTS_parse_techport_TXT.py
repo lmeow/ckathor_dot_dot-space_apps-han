cbd7155c950b14d256076f57874ffeadcf1464e6
Maja Frankiewicz <majafrank@live.se>
2021-10-03 22:09:59 +0200

    started parsing taxonomy

--- PROJECTS_parse_techport_TXT.py
+++ PROJECTS_parse_techport_TXT.py
@@ -1,7 +1,14 @@
 import json
 from types import new_class
 import re
+from neo4j import GraphDatabase
+from dotenv import dotenv_values
 
+config = dotenv_values(".env")
+uri = config.get('NEO4J_URI')
+user = config.get('NEO4J_USER')
+password = config.get('NEO4J_PASSWORD')
+driver = GraphDatabase.driver(uri, auth=(user, password))
 
 with open("techport_projects.txt", "r", encoding="utf-8") as f:
     for newline in f.readlines():
@@ -11,20 +18,92 @@ with open("techport_projects.txt", "r", encoding="utf-8") as f:
         project_description = ""
         project_startyear = ""
         project_endYear = ""
+        tax_nodes = ""
+        dest_nodes = ""
+        count_obj = '0'
 
         if 'projectId' in project:
             project_id = str(project['projectId'])
         if 'title' in project:
-            project_title = str(project['title'])
-        if'description' in project:
-            project_description = str(project['description'])
-        if'startYear' in project:
+            project_title = str(project['title']).replace('\"', '\\"').replace(
+                "'", "\\'")
+        if 'description' in project:
+            project_description = str(project['description']).replace('\"', '\\"').replace(
+                "'", "\\'")
+        if 'startYear' in project:
             project_startyear = str(project['startYear'])
-        if'endYear' in project:
+        if 'endYear' in project:
             project_endYear = str(project['endYear'])
 
-        project_node_query = "MERGE (n:TechPort:Project {tile:'"+project_title+"', projectId: '" + project_id+"', description: '" + \
+        project_node = "(n:TechPort:Project {tile: '"+project_title+"', projectId: '" + project_id+"', description: '" + \
             project_description + "', startYear: '" + project_startyear + \
             "', endYear: '" + project_endYear + "'})"
 
-        print(project_node_query)
+        project_node_query = "MERGE " + project_node
+
+        def run_project_queries(tx):
+            global project_node_query
+            print(project_node_query)
+            tx.run(project_node_query)
+
+        if len(project_node_query) > 0:
+            with driver.session() as session:
+                session.write_transaction(run_project_queries)
+
+        if 'primaryTaxonomyNodes' in project:
+            for tax in project['primaryTaxonomyNodes']:
+                tax_nodes += " MERGE (o"+count_obj + \
+                    ":TechPort:Taxonomy {"
+                for dict_attr, dict_value in tax.items():
+                    t = type(dict_value)
+                    if t == str or t == int:
+                        dict_value = str(dict_value)
+                        dict_attr = dict_attr.replace('-', '_')
+                        tax_nodes += dict_attr+":'" + \
+                            dict_value.replace('\"', '\\"').replace(
+                                "'", "\\'")+"', "
+                tax_nodes = tax_nodes[:-2] + \
+                    "}) MERGE (o"+count_obj + \
+                    ")<-[:HAS_TAXONOMY]-(n)"
+                tax_nodes = "MATCH " + project_node + tax_nodes
+
+                count_obj = str(int(count_obj) + 1)
+
+        # print(tax_nodes)
+
+                def run_tax_queries(tx):
+                    global tax_nodes
+                    print(tax_nodes)
+                    tx.run(tax_nodes)
+                if len(tax_nodes) > 0:
+                    with driver.session() as session:
+                        session.write_transaction(run_tax_queries)
+
+        if 'destinations' in project:
+            for destination in project['destinations']:
+                dest_nodes += " MERGE (p"+count_obj + \
+                    ":TechPort:Destination {"
+                for dict_attr, dict_value in destination.items():
+                    t = type(dict_value)
+                    if t == str or t == int:
+                        dict_value = str(dict_value)
+                        dict_attr = dict_attr.replace('-', '_')
+                        dest_nodes += dict_attr+":'" + \
+                            dict_value.replace('\"', '\\"').replace(
+                                "'", "\\'")+"', "
+                dest_nodes = dest_nodes[:-2] + \
+                    "}) MERGE (p"+count_obj + \
+                    ")<-[:HAS_DESTINATION]-(n)"
+
+                count_obj = str(int(count_obj) + 1)
+                dest_nodes = "MATCH " + project_node + dest_nodes
+
+                def run_destination_queries(tx):
+                    global dest_nodes
+                    print(dest_nodes)
+                    tx.run(dest_nodes)
+
+                if len(dest_nodes) > 0:
+
+                    with driver.session() as session:
+                        session.write_transaction(run_destination_queries)
---
+++ TAXONOMY_parse.py
@@ -0,0 +1,64 @@
+import requests
+import io
+from defusedxml.ElementTree import fromstring
+import os
+import json
+import uuid
+import pickle
+import codecs
+from neo4j import GraphDatabase
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+uri = config.get('NEO4J_URI')
+user = config.get('NEO4J_USER')
+password = config.get('NEO4J_PASSWORD')
+
+driver = GraphDatabase.driver(uri, auth=(user, password))
+
+r = requests.get('https://techport.nasa.gov/api/taxonomies/8816')
+r_tax = r.json()['taxonomy']['children']
+
+# print(r_tax)
+
+
+def rec_search(child_c):
+    if child_c['hasChildren']:
+        print('true')
+        r = requests.get(
+            'https://techport.nasa.gov/api/taxonomies/nodes/'+str(child_c['taxonomyNodeId']))
+        for child in r.json()['children']:
+            rec_search(child['content'])
+    else:
+        print('false')
+
+
+for a_child in r_tax:
+    rec_search(a_child['content'])
+
+# for child in r_tax:
+#     if child['hasChildren']:
+#         print('true')
+#         for child1 in child['children']:
+#             if child1['hasChildren']:
+#                 if child['hasChildren']:
+#                     print('true')
+#                 else:
+#                     print('false')
+#     else:
+#         print('false')
+# if 'destinations' in project:
+#     for destination in project['destinations']:
+#         dest_nodes += " MERGE (p"+count_obj + \
+#             ":TechPort:Destination {"
+# for dict_attr, dict_value in destination.items():
+#     t = type(dict_value)
+#     if t == str or t == int:
+#         dict_value = str(dict_value)
+#         dict_attr = dict_attr.replace('-', '_')
+#         dest_nodes += dict_attr+":'" + \
+#             dict_value.replace('\"', '\\"').replace(
+#                 "'", "\\'")+"', "
+# dest_nodes = dest_nodes[:-2] + \
+#     "}) MERGE (p"+count_obj + \
+#     ")<-[:HAS_DESTINATION]-(n)"
