8fa4b75fe7da7cc3fe36ac19839bfb85c3d410ab
Maja Frankiewicz <majafrank@live.se>
2021-10-03 18:21:56 +0200

    cleaned from comments

--- PROJECTS_parse_techport_TXT.py
+++ PROJECTS_parse_techport_TXT.py
@@ -23,32 +23,8 @@ with open("techport_projects.txt", "r", encoding="utf-8") as f:
         if'endYear' in project:
             project_endYear = str(project['endYear'])
 
-        # project_title = str(project['title'])
-        # project_description = str(project['description'])
-        # project_startyear = str(project['startYear'])
-        # project_endYear = str(project['endYear'])
-
         project_node_query = "MERGE (n:TechPort:Project {tile:'"+project_title+"', projectId: '" + project_id+"', description: '" + \
             project_description + "', startYear: '" + project_startyear + \
             "', endYear: '" + project_endYear + "'})"
 
         print(project_node_query)
-        # print(line)
-        # newline = line[:-2]
-
-        # newline = newline.replace("'", "!BAJS!")
-        # newline = newline.replace('"', "'")
-        # # newlime = re.sub("!BAJS!", '"', newline)
-        # newline = newline.replace("!BAJS!", '"')
-        # newline = json.dumps(newline, indent=4)
-        # print(newline)
-        # json_proj = json.loads(json.dumps(newline))
-        # outdata = eval(json_proj)
-
-        # print(type(json_proj))
-        # json_proj = json.loads(newline)
-        # print(type(json_proj))
-        # print(newline)
-
-        # skrivut = json.loads(newline)
-        # print(type(newline))
