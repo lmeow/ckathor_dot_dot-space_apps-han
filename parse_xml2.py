f822aabb4e2f0cefeab4cfab981310e31166e6ca
Maja Frankiewicz <majafrank@live.se>
2021-10-03 12:20:22 +0200

    Added for all tags

--- parse_xml2.py
+++ parse_xml2.py
@@ -1,7 +1,12 @@
-from bs4 import BeautifulSoup
 from lxml import etree
+import os
 
-xslt_root = etree.parse('./SKOS/competencies.xml')
+directory = './SKOS/'
+
+xslt_root = etree.parse('./SKOS/Access.skos')
+
+for filename in os.listdir(directory):
+    print(filename.replace('.skos', ''))
 
 concept_tag = "{http://www.w3.org/2004/02/skos/core#}Concept"
 preflabel_tag = "{http://www.w3.org/2004/02/skos/core#}prefLabel"
@@ -12,13 +17,16 @@ altLabel = ''
 
 for concept in xslt_root.findall(concept_tag):
     concept_children = concept.findall(".//")
+    # print(concept_children)
     for child in concept_children:
-        if child.tag == preflabel_tag:
-            preflabel = child.text
-            print('preflabel:', preflabel)
-        if child.tag == altLabel_tag:
-            altLabel = child.text
-            print('altLabel:', altLabel)
+        tags = child.tag.split('}')[1]
+        print(tags, child.text)
+        # if child.tag == preflabel_tag:
+        #     preflabel = child.text
+        #     print('preflabel:', preflabel)
+        # if child.tag == altLabel_tag:
+        #     altLabel = child.text
+        #     print('altLabel:', altLabel)
 
         # print(preflabel, altLabel)
         # print(xslt_root)
